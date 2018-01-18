2d6e19845700c2a209aaea683fe56534acb05844
chaoshengxiang <chaoshengxiang@sz-tripod.com.cn>
2018-01-18 09:35:39 +0800

    案例

---
+++ src/Test1/__init__.py
@@ -0,0 +1,12 @@
+from pip._vendor.distlib.compat import raw_input
+name =raw_input('what is your name?')
+if name.endswith('tank'):
+    print('Hello tank')
+elif name.endswith('xiao'):
+    print('Hello xiao')
+elif name.endswith('xiang'):
+    print('Hello xiang')
+elif name.endswith('xiang'):
+    print('Hello xiang')
+else:
+    print('Hello Strange')
